a3914121154f52bf63badba19599b250684731b5
Tamiris Beira <tamirisbeira@gmail.com>
2023-08-15 16:12:25 -0300

    refatorando código

--- Program.cs
+++ Program.cs
@@ -1,81 +1,49 @@
-﻿// Jogo de pergutas e respostas
-// Console.WriteLine() -> imprime as perguntas na tela
-// Recolher dados e salvar na variável
-// Mostrar pontuação para usuario
+﻿using System;
 
-int pontuacao = 0;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int pontuacao = 0;
 
-Console.WriteLine("Sua pontuação é:" +pontuacao+ "\nQuanto é 5 + 5?\nEscreva sua resposta abaixo:");
-int resp_1 = Convert.ToInt32(Console.ReadLine());
+        void VerificarResposta(string respostaCorreta, string respostaUsuario)
+        {
+            if (string.Equals(respostaUsuario, respostaCorreta, StringComparison.OrdinalIgnoreCase))
+            {
+                pontuacao++;
+            }
+            else
+            {
+                Console.WriteLine("Ops, você errou! A resposta correta é: " + respostaCorreta);
+                if (pontuacao > 0)
+                {
+                    pontuacao--;
+                }
+            }
+        }
 
-/*verifica se a resposta 1 está correta*/
-if (resp_1 == 10)
-{
-    pontuacao++;
-}
-else if (pontuacao <= 0)
-{
-    Console.WriteLine("Você perdeu!");
-}
-else
-{
-    Console.WriteLine("Ops você errou a resposta correta é: " + 10 + "você perdeu 1 ponto.");
-    pontuacao--;
-}
+        Console.WriteLine("Jogo de Perguntas e Respostas");
+        Console.WriteLine("Sua pontuação é: " + pontuacao);
 
-Console.WriteLine("Sua pontuação é:" + pontuacao + "\nQuanto é 7 x 9?\nEscreva sua resposta abaixo:");
-int resp_2 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Quanto é 5 + 5?\nEscreva sua resposta abaixo: ");
+        int resp_1 = Convert.ToInt32(Console.ReadLine());
+        VerificarResposta("10", resp_1.ToString());
 
-/*verifica se a resposta 2 está correta*/
-if (resp_2 == 63)
-{
-    pontuacao++;
-}
-else if (pontuacao <= 0)
-{
-    Console.WriteLine("Você perdeu!");
-}
-else
-{
-    Console.WriteLine("Ops você errou a resposta correta é: " + 10 + "você perdeu 1 ponto.");
-    pontuacao--;
-}
+        Console.WriteLine("Sua pontuação é: " + pontuacao);
+        Console.Write("Quanto é 7 x 9?\nEscreva sua resposta abaixo: ");
+        int resp_2 = Convert.ToInt32(Console.ReadLine());
+        VerificarResposta("63", resp_2.ToString());
 
-Console.WriteLine("Sua pontuação é:" + pontuacao + "\nQual é o nome do protagonista do jogo The Legend of Zelda: Breath of the Wild?\nEscreva sua resposta abaixo:");
-string resp_3 = Console.ReadLine();
+        Console.WriteLine("Sua pontuação é: " + pontuacao);
+        Console.Write("Qual é o nome do protagonista do jogo The Legend of Zelda: Breath of the Wild?\nEscreva sua resposta abaixo: ");
+        string resp_3 = Console.ReadLine();
+        VerificarResposta("Link", resp_3);
 
-/*verifica se a resposta 3 está correta*/
-if (resp_3 == "Link")
-{
-    pontuacao++;
-}
-else if (pontuacao <= 0)
-{
-    Console.WriteLine("Você perdeu!");
-}
-else
-{
-    Console.WriteLine("Ops você errou a resposta correta é: " + "Link" + "você perdeu 1 ponto.");
-    pontuacao--;
-}
+        Console.WriteLine("Sua pontuação é: " + pontuacao);
+        Console.Write("Em que jogo de videogame você pode encontrar um personagem chamado Mario?\nEscreva sua resposta abaixo: ");
+        string resp_4 = Console.ReadLine();
+        VerificarResposta("Super Mario Bros", resp_4);
 
-Console.WriteLine("Sua pontuação é:" + pontuacao + "\nEm que jogo de videogame você pode encontrar um personagem chamado Mario?\nEscreva sua resposta abaixo:");
-string resp_4 = Console.ReadLine();
-
-/*verifica se a resposta 4 está correta*/
-if (resp_4 == "Super Mario Bros")
-{
-    pontuacao++;
+        Console.WriteLine("Sua pontuação final é: " + pontuacao);
+    }
 }
-else if (pontuacao <= 0)
-{
-    Console.WriteLine("Você perdeu!");
-}
-else
-{
-    Console.WriteLine("Ops você errou a resposta correta é: " + "Super Mario Bros" + "você perdeu 1 ponto.");
-    pontuacao--;
-}
-
-
-
